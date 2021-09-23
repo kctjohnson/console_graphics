a6bc914ccfc9d9fbfa957c1f81827b4d55996c02
kctjohnson <killeanjohnson06@gmail.com>
2021-09-23 15:44:49 -0400

    Got different drawing methods working

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <curses.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include "Headers/vec.h"
 
@@ -12,103 +13,82 @@ struct Vec {
     int y;
 };
 
-// Current test version of rewriting the algo
-void BresenhamLine(int x1, int y1, int x2, int y2) {
-    int rise = y2 - y1;
-    int run = x2 - x1;
-    int slope = rise / run;
+// Based on https://medium.com/geekculture/dda-line-drawing-algorithm-be9f069921cf
+void DDALine(int x1, int y1, int x2, int y2) {
+    // Swap the start and end points if it's drawing from right to left
+    if (x1 > x2) {
+        std::swap(x1, x2);
+        std::swap(y1, y2);
+    }
+
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+
+    // Catch if the x values are the same, otherwise it causes divide by 0 error on slope
+    // Vertical line
+    if (dx == 0) {
+        for (int i = 0; i <= dy; i++) {
+            mvaddch(y1 + i, x1, '#');
+        }
+    } else {
+        double slope = (double)dy / (double)dx;
+
+        // Horizontal line
+        if (slope == 0) {
+            for (int x = x1; x <= x2; x++) {
+                mvaddch(y1, x, '#');
+            }
+        } else if (slope == 1) { // Diagonal line
+            for (int i = 0; i <= dx; i++) {
+                mvaddch(y1 + i, x1 + i, '#');
+            }
+        } else { // Any other line
+            for (int i = 0; i <= dx; i++) {
+                int y = (int)round(((double)y1 + ((double)i * slope)));
+                mvaddch(y, x1 + i, '#');
+            }
+        }
+    }
 }
 
-// Code from https://medium.com/geekculture/dda-line-drawing-algorithm-be9f069921cf
-void testDrawLine(int x1, int y1, int x2, int y2) {
-    const int dX = x2 - x1;
-    const int dY = y2 - y1;
-    const int absDX = abs(dX);
-    const int absDY = abs(dY);
+// Code from https://medium.com/geekculture/bresenhams-line-drawing-algorithm-2e0e953901b3
+void BresenhamLine(int x1, int y1, int x2, int y2) {
+    const int dx = x2 - x1;
+    const int dy = y2 - y1;
+    const int absdx = abs(dx);
+    const int absdy = abs(dy);
 
     int x = x1;
     int y = y1;
     mvaddch(y, x, '#');
 
-    if (absDX > absDY) {
-        int d = 2 * absDY - absDX;
-        for (int i = 0; i < absDX; i++) {
-            x = dX < 0 ? x - 1 : x + 1;
+    if (absdx > absdy) {
+        int d = 2 * absdy - absdx;
+        for (int i = 0; i < absdx; i++) {
+            // Are we going up a slope or down a slope?
+            x = dx < 0 ? x - 1 : x + 1;
             if (d < 0) {
-                d = d + 2 * absDY;
-            } else {
-                y = dY < 0 ? y - 1 : y + 1;
-                d = d + (2 * absDY - 2 * absDX);
+                d = d + 2 * absdy;
+            } else { // The decision variable has gone over 0, time to move along the y axis
+                // Are we going up a slope or down a slope?
+                y = dy < 0 ? y - 1 : y + 1;
+                d = d + (2 * absdy - 2 * absdx);
             }
             mvaddch(y, x, '#');
         }
     } else {
-        int d = 2 * absDX - absDY;
-        for (int i = 0; i < absDY; i++) {
-            y = dY < 0 ? y - 1 : y + 1;
+        int d = 2 * absdx - absdy;
+        for (int i = 0; i < absdy; i++) {
+            // Are we going up a slope or down a slope?
+            y = dy < 0 ? y - 1 : y + 1;
             if (d < 0) {
-                d = d + 2 * absDX;
-            } else {
-                x = dX < 0 ? x - 1 : x + 1;
-                d = d + (2 * absDX - 2 * absDY);
+                d = d + 2 * absdx;
+            } else { // The decision variable has gone over 0, time to move along the y axis
+                // Are we going up a slope or down a slope?
+                x = dx < 0 ? x - 1 : x + 1;
+                d = d + (2 * absdx - 2 * absdy);
             }
             mvaddch(y, x, '#');
-        }
-    }
-}
-
-// Implementation based heavily on https://classic.csunplugged.org/wp-content/uploads/2014/12/Lines.pdf
-// Currently not working
-void drawline(Vec p1, Vec p2) {
-    int deltaX = p2.x - p1.x;
-    int deltaY = p2.y - p1.y;
-
-    p2.x += sign(deltaX);
-    p2.y += sign(deltaY);
-
-    int A, B, P, lineDirection;
-    if (abs(deltaY) > abs(deltaX)) {
-        A = 2 * abs(deltaX);
-        B = A - (abs(2 * deltaY));
-        P = A - abs(deltaY);
-        lineDirection = deltaY > 0 ? 1 : -1;
-    } else if (abs(deltaX) == abs(deltaY)) {
-        A = 2 * abs(deltaY);
-        B = A - (abs(2 * deltaX));
-        P = A - abs(deltaX);
-        lineDirection = sign(deltaY);
-    } else {
-        A = 2 * abs(deltaY);
-        B = A - (abs(2 * deltaX));
-        P = A - abs(deltaX);
-        lineDirection = deltaX > 0 ? 1 : -1;
-    }
-
-    mvaddch(p1.y, p1.x, '#');
-
-    if (abs(deltaY) > abs(deltaX)) {
-        int curX = p1.x;
-        for (int curY = p1.y; curY != p2.y; curY += sign(deltaY)) {
-            if (P < 0) {
-                mvaddch(curY, curX, '#');
-                P += A;
-            } else {
-                curX += lineDirection;
-                mvaddch(curY, curX, '#');
-                P += B;
-            }
-        }
-    } else {
-        int curY = p1.y;
-        for (int curX = p1.x; curX != p2.x; curX += sign(deltaX)) {
-            if (P < 0) {
-                mvaddch(curY, curX, '#');
-                P += A;
-            } else {
-                curY += lineDirection;
-                mvaddch(curY, curX, '#');
-                P += B;
-            }
         }
     }
 }
@@ -121,17 +101,22 @@ int main() {
     Vec p3 = { .x = 5, .y = 10 };
     Vec p4 = { .x = 0, .y = 5 };
 
-    testDrawLine(p1.x, p1.y, p2.x, p2.y);
+    /* DDALine(0, 0, 5, 5); */
+    /* DDALine(7, 0, 7, 5); */
+    /* DDALine(15, 0, 9, 0); */
+    /* getch(); */
+
+    BresenhamLine(p1.x, p1.y, p2.x, p2.y);
     getch();
-    testDrawLine(p2.x, p2.y, p3.x, p3.y);
+    BresenhamLine(p2.x, p2.y, p3.x, p3.y);
     getch();
-    testDrawLine(p3.x, p3.y, p4.x, p4.y);
+    BresenhamLine(p3.x, p3.y, p4.x, p4.y);
     getch();
-    testDrawLine(p4.x, p4.y, p1.x, p1.y);
+    BresenhamLine(p4.x, p4.y, p1.x, p1.y);
     getch();
 
-    vec2d newvec(10, 5);
-    mvaddstr(1, 1, newvec.toString().c_str());
+    //vec2d newvec(10, 5);
+    //mvaddstr(1, 1, newvec.toString().c_str());
 
     refresh();
     getch();
